7961902bbf8cd9de1b903a3171ca43cfb1c7cb6a
rice8y <eitoy1128@mbr.nifty.com>
2024-08-21 00:58:41 +0900

    commit

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -18,6 +18,12 @@ const projectsData: Project[] = [
     imgSrc: '/static/images/sqlite-m.png',
     href: 'https://rice8y.github.io/sqlite3/',
   },
+  {
+    title: 'ごみカレンダー bot',
+    description: '道後・湯築地区のごみカレンダー bot.',
+    imgSrc: '/static/images/trash.png',
+    href: 'https://l.instagram.com/?u=https%3A%2F%2Fliff.line.me%2F1645278921-kWRPP32q%2F%3FaccountId%3D756viccf&e=AT2ZQcKBYVa1OchuUomVezPyvR1otSp2w85DUGAz4QTiv6NdQgKsMOVIXfC00jiHeBgjIrTHnCMTEEoHRiaxFxctMcmFpsnFHfUPO-A',
+  },
 ]
 
 export default projectsData
