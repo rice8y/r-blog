20ef71892a649f15e12be9e549513beade3b40fd
rice8y <eitoy1128@mbr.nifty.com>
2024-08-31 23:09:56 +0900

    update

---
+++ components/GoogleMap.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const GoogleMap = ({ src, width, height }) => (
+  <iframe
+    src={src}
+    width={width}
+    height={height}
+    style={{ border: 0 }}
+    allowFullScreen={true}
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+  ></iframe>
+);
+
+export default GoogleMap;
