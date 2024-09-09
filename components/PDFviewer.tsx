a4c8d11d0010f68b1f77a572cbf397f0c93b03d8
rice8y <eitoy1128@mbr.nifty.com>
2024-09-10 08:29:39 +0900

    update

--- components/PDFviewer.tsx
+++ components/PDFviewer.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
-const PDFviewer = ({ pdfSrc }) => {
+interface PDFViewerProps {
+  pdfSrc: string
+}
+
+const PDFViewer: React.FC<PDFViewerProps> = ({ pdfSrc }) => {
   const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
@@ -17,15 +21,18 @@ const PDFviewer = ({ pdfSrc }) => {
   }, [])
 
   return isMobile ? (
-    <a href={pdfSrc}>PDF Download</a>
+    <a href={pdfSrc} style={{ display: 'block', textAlign: 'center', margin: '20px' }}>
+      PDF Download
+    </a>
   ) : (
     <iframe
       width="100%"
       height="400px"
       src={pdfSrc}
       title="PDF Viewer"
+      style={{ border: 'none' }}
     />
   )
 }
 
-export default PDFviewer
+export default PDFViewer
